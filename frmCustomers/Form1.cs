92658b3207bbc4adb5efb691322ad14e8fc57a48
Sundar Raj <sundar.raj@oneadvanced.com>
2017-12-05 13:07:20 +0530

    change

--- frmCustomers/Form1.cs
+++ frmCustomers/Form1.cs
@@ -19,7 +19,7 @@ namespace frmCustomers
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //Form load code here
+            //Form load code here has changed test this.
         }
     }
 }
