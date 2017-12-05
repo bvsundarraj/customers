9fefc6f83e8d6fc5f760c61c6cd11db61b304cbf
Sundar Raj <sundar.raj@oneadvanced.com>
2017-12-05 12:33:01 +0530

    comment added to form load

--- frmCustomers/Form1.cs
+++ frmCustomers/Form1.cs
@@ -16,5 +16,10 @@ namespace frmCustomers
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Form load code here
+        }
     }
 }
